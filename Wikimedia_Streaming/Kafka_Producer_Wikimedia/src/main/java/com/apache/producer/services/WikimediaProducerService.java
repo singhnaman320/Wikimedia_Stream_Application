bc43bbefbe6cb25ff3e506cf14540b2bf87708dc
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-13 15:02:34 +0530

    Adding Wikimedia producer service

--- Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/services/WikimediaProducerService.java
+++ Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/services/WikimediaProducerService.java
@@ -1,9 +1,13 @@
 package com.apache.producer.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WikimediaProducerService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaProducerService.class);
+	
 	
 }
