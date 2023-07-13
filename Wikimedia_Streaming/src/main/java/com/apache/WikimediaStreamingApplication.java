9f30defbfd6d40dc5ffa304bbd513aa82b048dcb
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-13 12:24:04 +0530

    Adding Wikimedia app basic structure

---
+++ Wikimedia_Streaming/src/main/java/com/apache/WikimediaStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikimediaStreamingApplication.class, args);
+	}
+
+}
