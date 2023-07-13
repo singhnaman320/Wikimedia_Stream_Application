054eac413802c5fdbd5673768afc42f6cb24647b
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-13 18:16:17 +0530

    Adding Kafka Consumer Wikimedia module

--- Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/SpringBootProducerApplication.java
+++ Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/SpringBootProducerApplication.java
@@ -1,13 +1,27 @@
 package com.apache.producer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.apache.producer.services.WikimediaProducerService;
+
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBootProducerApplication.class, args);
+	}
+
+	@Autowired
+	private WikimediaProducerService wikimediaProducerService;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		wikimediaProducerService.sendMessage();
 	}
 }
