01700047c3de075f6c4292deb096407e338aabf5
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-13 13:15:22 +0530

    Adding Kafka_Producer_Wikimedia tpoic

--- Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/config/KafkaTopicConfig.java
+++ Wikimedia_Streaming/Kafka_Producer_Wikimedia/src/main/java/com/apache/producer/config/KafkaTopicConfig.java
@@ -1,5 +1,16 @@
 package com.apache.producer.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaTopicConfig {
 
+	public NewTopic creatingTopic() {
+		
+		return TopicBuilder
+				.name("wikimediaStream")
+				.build();
+	}
 }
