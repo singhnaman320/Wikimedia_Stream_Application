38ac9f56c1a7507d0b0bb589168dbf4f2b7ffc8d
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-14 13:02:36 +0530

    Adding WikimediaData entity

--- Wikimedia_Streaming/Kafka_Consumer_Wikimedia/src/main/java/com/apache/consumer/model/WikimediaData.java
+++ Wikimedia_Streaming/Kafka_Consumer_Wikimedia/src/main/java/com/apache/consumer/model/WikimediaData.java
@@ -4,14 +4,28 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "wikimedia")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class WikimediaData {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long wikId;
+	
+	@Lob // In order to store large data we use @Lob
 	private String wikiEventData;
 }
